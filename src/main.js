f44e02b2fd51e6a0296544cb3a625185b2a7997c
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2021-12-03 13:27:43 -0300

    Running landlord migration

--- src/main.js
+++ src/main.js
@@ -24,8 +24,10 @@ module.exports = (vemto) => {
             vemto.log.info(`Configuring Multi-Tenancy`)
 
             this.publishConfigFile()
-
+            this.editConfigFile()
             this.addDatabaseConnections()
+            this.publishLandlordMigration()
+            this.migrateLandlordDatabase()
         },
 
         publishConfigFile() {
@@ -41,11 +43,35 @@ module.exports = (vemto) => {
             // Register the file to prevent Vemto overwriting it after manual changes
             vemto.registerProjectFile('/config/multitenancy.php')
         },
+
+        editConfigFile() {
+            if(!vemto.projectFileExists('/config/multitenancy.php')) return
+
+            let configFileContent = vemto.readProjectFile('/config/multitenancy.php')
+
+            if(configFileContent.includes(`Spatie\\Multitenancy\\Tasks\\SwitchTenantDatabaseTask::class`)) {
+                vemto.log.warning('Multi-Tenancy configuration already modified. Skipping...')
+                return
+            }
+
+            configFileContent = configFileContent.replace(`'tenant_database_connection_name' => null`, `'tenant_database_connection_name' => 'tenant'`)
+            configFileContent = configFileContent.replace(`'landlord_database_connection_name' => null`, `'landlord_database_connection_name' => 'landlord'`)
+
+            let phpParser = vemto.parsePhp(configFileContent)
+            phpParser.addItemToNestedNamedArray('switch_tenant_tasks', 'Spatie\\Multitenancy\\Tasks\\SwitchTenantDatabaseTask::class')
+
+            let newConfigFileContent = vemto.formatByLanguage(phpParser.getCode(), 'php')
+
+            vemto.writeProjectFile('/config/multitenancy.php', newConfigFileContent)
+
+            // Register the file to prevent Vemto overwriting it after manual changes
+            vemto.registerProjectFile('/config/multitenancy.php')
+        },
  
         addDatabaseConnections() {
             let projectConnectionType = vemto.getProject().connection.type, // mysql, pgsql, etc
 
-                databaseFileContent = vemto.readProjectFile('config/database.php'),
+                databaseFileContent = vemto.readProjectFile('/config/database.php'),
                 multitenancySettings = vemto.readPluginFile(`files/${projectConnectionType}-settings.txt`)    
             
             if(databaseFileContent.includes(`'tenant' => [`)) {
@@ -59,10 +85,27 @@ module.exports = (vemto) => {
 
             let newDatabaseConfigContent = vemto.formatByLanguage(phpParser.getCode(), 'php')
 
-            vemto.writeProjectFile('config/database.php', newDatabaseConfigContent)
+            vemto.writeProjectFile('/config/database.php', newDatabaseConfigContent)
 
             // Register the file to prevent Vemto overwriting it after manual changes
-            vemto.registerProjectFile('config/database.php')
+            vemto.registerProjectFile('/config/database.php')
+        },
+
+        publishLandlordMigration() {
+            if(vemto.projectFolderExists('/database/migrations/landlord')) {
+                vemto.log.warning('Multi-Tenancy Landlord migration already generated. Skipping...')
+                return
+            }
+
+            let command = `vendor:publish --provider="Spatie\\Multitenancy\\MultitenancyServiceProvider" --tag="migrations"`
+
+            vemto.executeArtisan(command)
+        },
+
+        migrateLandlordDatabase() {
+            let command = `migrate --path=database/migrations/landlord --database=landlord`
+
+            vemto.executeArtisan(command)
         }
 
     }
